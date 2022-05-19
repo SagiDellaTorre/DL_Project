1525fc362d1ddaf17835922d891aaedbbde6e6e8
Sagi <you@example.com>
2022-05-19 13:53:08 +0300

    rename file by order in folder

---
+++ src/tests/rename_files_by_order.py
@@ -0,0 +1,26 @@
+import os
+
+def rename_files_by_oreder(path):
+
+    new_index = 0
+    file_first = "/file_"
+    file_end = ".csv"
+
+    for i in range(3000):
+        old_name = path + file_first + str(i) + file_end
+        new_name = path + file_first + str(new_index) + file_end
+        if os.path.exists(old_name):
+            if i != new_index:
+                os.rename(old_name, new_name)
+            else:
+                new_index += 1
+        else:
+            continue
+
+def main():
+    
+    rename_files_by_oreder()
+
+if __name__ == '__main__':
+
+    main()
